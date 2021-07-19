1fd32a439e36c18f2c2180a09091e04a78b9ed0c
MEnghi44 <69116659+MEnghi44@users.noreply.github.com>
2021-07-19 15:51:58 +0700

    U

--- src/com/journaldev/composition/TestPerson.java
+++ src/com/journaldev/composition/TestPerson.java
@@ -1,5 +1,10 @@
 package com.journaldev.composition;
 
 public class TestPerson {
+	
+	public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+    }
 
 }
